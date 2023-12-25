0303a1a1d759e408a79c4e35b3286e50a597b284
Ali <zaabat97@gmail.com>
2023-12-26 00:38:56 +0100

    wip database connection

--- front/src/app/authentification/authentification.component.ts
+++ front/src/app/authentification/authentification.component.ts
@@ -6,15 +6,12 @@ import {MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import { confirmPasswordValidator} from './confirmPasswordValidator.directive';
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from "ng-recaptcha";
-
 @Component({
   selector: 'app-authentification',
   templateUrl: './authentification.component.html',
   styleUrls: ['./authentification.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, CommonModule, MatButtonModule, MatIconModule, RecaptchaV3Module ],
-  providers: [{ provide: RECAPTCHA_V3_SITE_KEY, useValue: "6Lf99B0pAAAAADLeP621ZGRnr8Bve19fEeuuSpwN" }],
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, CommonModule, MatButtonModule, MatIconModule ],
 })
 export class AuthentificationComponent {
   hide = true;
@@ -23,7 +20,6 @@ export class AuthentificationComponent {
       email: new FormControl('', [Validators.required, Validators.email]),
       password :  new FormControl('', [Validators.required, Validators.minLength(10), Validators.pattern(/^(?=.*[A-Z]).*(?=.*[a-z]).*(?=.*[!@#$%^&*()_+\-=\[\]{};',.:\/?]).{10,}$/)]),
       confirmPassword: new FormControl('', [Validators.required]),
-      recaptchaReactive: new FormControl(null, Validators.required),
     }, {validators: confirmPasswordValidator});
   onSubmit() {
       console.log('data', this.form?.value);
