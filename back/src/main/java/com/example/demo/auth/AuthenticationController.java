69054a1ffdfb885e2ead75770f9d829615447ece
Ali <zaabat97@gmail.com>
2024-01-05 17:58:06 +0100

    fixing Enabling Cross Origin Requests done

--- back/src/main/java/com/example/demo/auth/AuthenticationController.java
+++ back/src/main/java/com/example/demo/auth/AuthenticationController.java
@@ -7,13 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-@CrossOrigin(origins = "http://localhost:8000")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
-
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
