9dedc7b06714d1de82849c0c1e4b73040c42d502
Ali <zaabat97@gmail.com>
2024-01-10 20:10:35 +0100

    adding dashboard, blogForm and register routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FormBlogComponent} from "./components/form-blog/form-blog.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {AuthenticationComponent} from "./components/authentication/authentication.component";
 const routes: Routes = [
-  { path: 'blogForm', component: FormBlogComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'blogForm', component: FormBlogComponent },
+  { path: 'register', component: AuthenticationComponent },
 ];
 
 @NgModule({
